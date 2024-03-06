85338b74b7aed0338891677e7ed2dfdf1411a808
Sushant Kumbhar <142664990+sushant8887@users.noreply.github.com>
2024-03-06 09:35:25 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectMongo =()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/comp')
-.then(()=>console.log('connected to mongodb..'))
-.catch((err)=>console.log(err))
-};
-
-module.exports=connectMongo;
